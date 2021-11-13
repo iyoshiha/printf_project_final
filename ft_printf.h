684c1982b049c99a7b806e9bdd2943f17e80cf99
Ito Yoshiharu <iyoshiha@c1r20s7.42tokyo.jp>
2021-11-14 04:20:29 +0900

    printf.c structured

--- ft_printf.h
+++ ft_printf.h
@@ -1,28 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/14 03:01:04 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 04:19:48 by iyoshiha         ###   ########.fr       */
+/*   Created: 2021/11/14 03:07:53 by iyoshiha          #+#    #+#             */
+/*   Updated: 2021/11/14 03:22:11 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include <stdarg.h>
 
-int	ft_printf(const	char* format, ...)
-{
-	va_list	ap;
-	int		printed_len;
-
-	va_start(ap, format);
-	outputAndCount(format, &printed_len, ap);
-	va_end(ap);
-
-
-
-	return (printed_len);	// excluding the null byte ('\0') used to end output to strings.
-						 	// negative number returned when error happened.
-}
+int	ft_printf(const	char* format, ...);
