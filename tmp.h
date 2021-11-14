644f7c7d9ba52fb2f20caf2d061aea5cdebef2c4
Ito Yoshiharu <iyoshiha@c1r20s7.42tokyo.jp>
2021-11-14 14:54:30 +0900

    define

--- tmp.h
+++ tmp.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 04:39:14 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 13:03:14 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/14 14:54:01 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include "hhhh/libft.h"
 #
 #define NOTEOA(chr)	((chr=='\0')==0)
+# define FORMAT	data->format
+# define INDEX	data->format_i
 # define CONVERSION_MATCHED(conv)	((data->format[data->format_i]) == ('conv')) handle_conv();
 # define CONVERSION_MATCHED_X 		(data->format[data->format_i]) == ('X') handle_X()
 typedef struct	s_formatData
