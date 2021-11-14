84183df825e426000333ee260a0911d02a5cc05a
Ito Yoshiharu <iyoshiha@c1r20s7.42tokyo.jp>
2021-11-14 15:59:43 +0900

    pct

--- handle/handle_%.c
+++ handle/handle_%.c
@@ -6,17 +6,17 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 15:46:09 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 15:49:00 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/14 15:59:12 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../tmp.h"
 
-void	handle_(t_formatData* data)
+void	handle_pct(t_formatData* data) // pct = percentage
 {
 	char chr;
 
-	chr = va_arg(data->ap, char);
+	chr = '%';
 	ft_putchar_fd(chr, STDOUT_FILENO);
 	ADD LEN_PRINTED++;
 	return ;
