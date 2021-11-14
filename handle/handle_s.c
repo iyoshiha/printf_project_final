2e2c60dd196ee6c1077683d203e765453ab48c5f
Ito Yoshiharu <iyoshiha@c1r20s7.42tokyo.jp>
2021-11-14 16:23:29 +0900

    mv libft ..

--- handle/handle_s.c
+++ handle/handle_s.c
@@ -6,7 +6,18 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:06:43 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 16:15:54 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/14 16:18:02 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../tmp.h"
+
+void	handle_s(t_formatData* data)
+{
+	char ;
+
+	chr = va_arg(data->ap, char);
+	ft_putchar_fd(chr, STDOUT_FILENO);
+	ADD LEN_PRINTED++;
+	return ;
+}
