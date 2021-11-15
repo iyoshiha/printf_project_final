f9083e3f3aa5b5daae62ae9c876c17873baf5562
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-16 05:06:59 +0900

    hexbase base strucrure

--- component/printHexBase copy.c
+++ component/printHexBase copy.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:08:08 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/16 04:18:34 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/16 05:06:27 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,22 @@ int		printAddress(void* ptr)
 
 	ft_putstr_fd("0x", STDOUT_FILENO);
 	total_num_printed = ft_strlen("0x");
-	total_num_printed = printHexBase(ptr);
+	total_num_printed = printHexBase((long long int)ptr);
 	return (total_num_printed);
 }
 
-int		printHexBase()
+int		printHexBase(long long int decimal)
 {
+	int	next_digit;
+	int remainder;
+	static int count;
+
+	// handle deciaml =< 0;
+	if (next_digit = decimal / 16 > 0)
+		printHexBase(next_digit);
+	ft_putchar_fd (remainder = decimal % 16, STDOUT_FILENO);
+	return(count++);
+
+
 
 }
--- prac/main2.c
+++ prac/main2.c
@@ -4,9 +4,14 @@
 # include <string.h>
 # include "../header/libft.h"
 
+int f(long long int l)
+{
+	puts("SUCCESS: cast to long long int as arg from void *");
+	return (l);
+}
 void h(void* ptr)
 {
-	printf("%p\n", ptr);
+	printf("in h func : %p\n", ptr);
 	return ;
 }
 
@@ -14,11 +19,14 @@ int	main(void)
 {
 	void* num;
 	long	l;
+	void* ptr;
 
 	num = &num;
-	l = (long)num;  // SUCCESS: cast to long int from point type
-	printf("%lx\n", l);
+	ptr = &l;
+	printf("%xl\n", f((long long int)ptr));
+	// l = (long)num;  // SUCCESS: cast to long int from point type
+	// printf("%lx\n", l);
 	h((num));
-	printf("hello %p\n", num);
+	// printf("hello %p\n", num);
 	return(0);
 }
