5b4f832039fb80c209b3537e3184a8c61b2eb9d4
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-18 04:15:33 +0900

    moved files to component from prac

--- component/component.h
+++ component/component.h
@@ -6,13 +6,13 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 06:11:45 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/16 06:15:16 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/18 02:33:14 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		printAddress(void* ptr);
 int		printHexBase(long long unsigned int decimal);
-int		replace_ten2a(num);
+int		replace_ten2a(int num);
 int		print0(void);
 
 
--- component/printAddress.c
+++ component/printAddress.c
@@ -1,16 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printHexBase copy.c                                :+:      :+:    :+:   */
+/*   printAddress.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 21:08:08 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/16 05:58:13 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/18 04:11:35 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../tmp.h"
+# include "component.h"
+
 
 // print number by hexadeciam base
 // return the total number of printed charactors.
@@ -22,40 +24,6 @@ int		printAddress(void* ptr)
 		return(print0());
 	ft_putstr_fd("0x", STDOUT_FILENO);
 	total_num_printed =	LENGTH_OF_0X;
-	total_num_printed = printHexBase((long long unsigned int)ptr);
+	total_num_printed += printHexBase((long long unsigned int)ptr);
 	return (total_num_printed);
-}
-
-int		printHexBase(long long unsigned int decimal)
-{
-	int	next_digit;
-	int remainder;
-	static int count;
-
-	if (decimal == 0)
-		return(print0());
-	if (next_digit = decimal / 16 > 0)
-		printHexBase(next_digit);
-	remainder = decimal % 16;
-	if (remainder => 10)
-		replace_ten2a(remainder);
-	else
-		remainder += '0'
-	ft_putchar_fd (remainder, STDOUT_FILENO);
-	return(count++);
-}
-
-int		replace_ten2a(num)
-{
-	int base;
-
-	base = 'a' - 10;
-	return (base + num);
-}
-
-int		print0(void)
-{
-	int		printed_num;
-	ft_putchar_fd("0", STDOUT_FILENO);
-	return (printed_num = 1);
 }
