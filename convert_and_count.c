00f42ca4fd0008f254174a17b4739719d977d328
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-18 23:21:59 +0900

    bug fixed

--- convert_and_count.c
+++ convert_and_count.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 05:50:26 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/18 22:27:40 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/18 23:20:07 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,20 @@
 
 void	ConvertAndCount(t_formatData* data)
 {
-	if _PF_CONV_MATCHED(c)
+	if _PF_CONV_MATCHED('c')
 		handle_c(data);
-	if _PF_CONV_MATCHED(s)
+	if _PF_CONV_MATCHED('s')
 		handle_s(data);
-	if _PF_CONV_MATCHED(p)
+	if _PF_CONV_MATCHED('p')
 		handle_p(data);
-	if _PF_CONV_MATCHED(d)
+	if _PF_CONV_MATCHED('d')
 		handle_d(data);
-	if _PF_CONV_MATCHED(i)
+	if _PF_CONV_MATCHED('i')
 		handle_i(data);
-	if _PF_CONV_MATCHED(u)
+	if _PF_CONV_MATCHED('u')
 		handle_u(data);
 	if _PF_CONV_MATCHED_LOW_X
 	if _PF_CONV_MATCHED_CAP_X
-	if _PF_CONV_MATCHED(%)
+	if _PF_CONV_MATCHED('%')
 		handle_pct(data);
-
 }
