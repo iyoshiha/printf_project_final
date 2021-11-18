7fadc7126fd7cb7153ea40b787faf98d8c14c1f2
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-18 19:29:39 +0900

    done i (the same as d )

--- handle/handle_i.c
+++ handle/handle_i.c
@@ -6,7 +6,19 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:06:37 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 16:15:58 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/18 19:28:56 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../component/component.h"
+# include "../tmp.h"
+
+int		handle_i(t_formatData* data)
+{
+	int		decimal;
+	int 	printed_num;
+
+	decimal = va_arg(data->ap, int);
+	printed_num = printDecimalBase(decimal, STDOUT_FILENO);
+	return (printed_num);
+}
--- handle/handle_u.c
+++ handle/handle_u.c
@@ -6,7 +6,19 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:06:46 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 16:15:55 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/18 19:18:26 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../component/component.h"
+# include "../tmp.h"
+
+int		handle_u(t_formatData*	data)
+{
+	unsigned int	unsigned_decimal;
+	int				printed_num;
+
+	unsigned_decimal = va_arg(data->ap, unsigned);
+	printed_num = printUnsignedDecimal(unsigned_decimal, STDOUT_FILENO);
+	return (printed_num);
+}
