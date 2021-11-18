19c5595ede5747ed1cc472d9ed5cd8816bce0dbf
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-19 00:27:43 +0900

    done !!

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 int main()
 {
-	printf("%d\n", _PHEX_CAPITAL);
-	ft_printf("%d\n", _PHEX_CAPITAL);
+printf("printed num : %d  :output:", 	printf("%d\n", 12938));
+printf("printed num : %d  :output:", 	ft_printf("%d\n", 12938));
+printf("printed num : %d  :output:", 	printf("%s\n", "hell no"));
+printf("printed num : %d  :output:", 	ft_printf("%s\n", "hell no"));
+printf("printed num : %d  :output:", 	printf("%c\n", 'A'));
+printf("printed num : %d  :output:", 	ft_printf("%c\n", 'A'));
+printf("printed num : %d  :output:", 	printf("%x\n", 22222));
+printf("printed num : %d  :output:", 	ft_printf("%x\n", 22222));
+printf("printed num : %d  :output:", 	printf("%X\n", 11111));
+printf("printed num : %d  :output:", 	ft_printf("%X\n", 11111));
 	return 0;
 }
