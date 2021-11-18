6febaa573555e9d0fcdbb0a585bf6a98917c6112
Ito Yoshiharu <iyoshiha@c1r27s5.42tokyo.jp>
2021-11-18 09:53:20 +0900

    fuck

--- handle/handle_x.c
+++ handle/handle_x.c
@@ -5,8 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/14 16:06:49 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/14 16:15:52 by iyoshiha         ###   ########.fr       */
+/*   Created: 2021/11/14 16:06:23 by iyoshiha          #+#    #+#             */
+/*   Updated: 2021/11/18 09:52:54 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../component/component.h"
+#include "../tmp.h"
+
+void	handle_x(t_formatData data)
+{
+	unsigned int	ui;
+
+	ui = va_arg(data->ap, unsigned int);
+	LEN_PRINTED += print_unsigned_int_hex(ui);
+	return ;
+}
+
+void	print_unsigned_int_hex(unsigned int decimal)
+{
+	 printHexBase(decimal);
+	 return ;
+}
