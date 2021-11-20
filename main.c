05c12b25c9287e6fb2d8fe58d213f45fe3265ac2
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-20 15:33:25 +0900

    done

--- main.c
+++ main.c
@@ -5,11 +5,17 @@
 
 int main()
 {
-	int p;
-	p = 10;
-	int count[100];
-	int	i = 0;
-	//
+	// int int p;
+	// int p = 10;
+	// int count[100];
+	// int	i = 11;
+	char s[] = "noone";
+
+		printf("%u", s);
+	    // printf("%010.3d:\n", i);
+		// printf("%-010.3d:\n", i);
+
+	/*
 	count[i++] = ft_printf("%p", &i);
 	count[i++] = ft_printf("%x\n", 0xabc);
 	count[i++] = ft_printf("\n%s\n", "abc");
@@ -19,7 +25,7 @@ int main()
 	count[i++] = ft_printf("%i\n", 1234);
 	count[i++] = ft_printf("%u\n", 0xFFFFFFFF);
 	count[i++] = ft_printf("%X\n", 0xFBA); // bug returns 11 but should be 8
-	//
+	*/
 	// count[i++] = ft_printf("%X\n%X\n", 0xFBA,0xFBA); // bug returns 11 but should be 8
 /*
 	count[i++] = ft_printf("%X", 0xFBA,0xFBA);
@@ -27,10 +33,12 @@ int main()
 	count[i++] = ft_printf("%X", 0xFBA,0xFBA);
 	puts("\n");
 	end = i;
-	*/
+	========
 	printf("x:%d p: %d p:%d X:%d ", count[0], count[1], count[2], count[3]);
 	printf("x:%d p: %d p:%d X:%d ", count[4], count[5], count[6], count[7]);
 	// printf("X:%d ||  X:%d", count[0], count[1]);
+	=======
+	*/
 	/*
 	printf("printed num : %d  :output:\n", printf("%d\n", 12938));
 	printf("printed num : %d  :output:\n", ft_printf("%d\n", 12938));
