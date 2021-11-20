fcc9b72a74f257815e1afc9bd964f8eb9e1cd4c4
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-20 16:30:33 +0900

    ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 03:01:04 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/18 21:43:05 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/20 16:29:41 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ int	ft_printf(const	char* format, ...)
 	va_start(_PF_AP, format);
 	outputAndCount(data);
 	va_end(_PF_AP);
-	return (data->printed_len);	// excluding the null byte ('\0') used to end output to strings.
+	return (_PF_LEN_PRINTED);	// excluding the null byte ('\0') used to end output to strings.
 						 	// negative number returned when error happened.
 }
