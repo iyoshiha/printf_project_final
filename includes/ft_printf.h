d271d7c7aa5a0380cdd5df47f03da5bd85312085
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-21 15:14:11 +0900

    s

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 04:39:14 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/21 14:42:41 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/21 15:14:03 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,6 @@
 # include <stdarg.h>
 # include "libft.h"
 
-# define _PF_NOTEOS(chr)	((chr=='\0')==0)
-# define _PF_FORMAT	data->format
-# define _PF_INDEX	data->format_i
-# define _PF_AP		data->ap
-# define _PF_LEN_PRINTED	data->printed_len
-# define _PF_CONV_MATCHED(conv)
-# define _PF_CONV_MATCHED_LOW_X 	((data->format[data->format_i]) == ('x')) ret = handle_lower_x(data);
-# define _PF_CONV_MATCHED_CAP_X 	((data->format[data->format_i]) == ('X')) ret = handle_large_x(data);
 # define _NEXT
 # define _ADD
 
