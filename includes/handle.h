aff7ccfeea1650e154ee87102cf7b38500eae973
Ito Yoshiharu <iyoshiha@c1r29s7.42tokyo.jp>
2021-11-21 15:36:59 +0900

    d

--- includes/handle.h
+++ includes/handle.h
@@ -6,14 +6,14 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 22:01:48 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/21 15:33:39 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/21 15:36:39 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HANDLE_H
 # define HANDLE_H
 
-#define "libft.h"
+# define "libft.h"
 
 int		handle_pct(t_formatData* data);
 int		handle_c(t_formatData* data);
