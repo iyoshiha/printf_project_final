df2ddb45658c4bb36aa8e7e9cd9120d49352b77e
Ito Yoshiharu <iyoshiha@c1r31s8.42tokyo.jp>
2021-11-25 04:34:57 +0900

    dn hex header

--- includes/printHexBase.h
+++ includes/printHexBase.h
@@ -6,7 +6,7 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 06:11:45 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/24 03:05:39 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/25 04:34:31 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # define PHEX_LOWERCASE 0
 # define PHEX_LEN_OF_0X	2
 
-int		printAddress(void* ptr);
-int		printHexBase(long long unsigned int decimal, char flag);
+int		printAddress(void	*ptr);
+int		printHexBase(long long unsigned int	decimal, char	flag);
 int		replace_ten2a(int num);
 int		replace_ten2a_capital(int num);
 int		print0(void);
@@ -26,4 +26,4 @@ int		print_unsigned_int_hex(unsigned int decimal);
 int		printDecimalBase(int n, int fd);
 int		printUnsignedDecimal(unsigned int n, int fd);
 
-# endif
+#endif
