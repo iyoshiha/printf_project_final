68b6e136446490dc14f5e081266be716e27bdddd
Ito Yoshiharu <iyoshiha@c1r31s8.42tokyo.jp>
2021-11-25 04:14:27 +0900

    done hex

--- hex_component/printHexBase.c
+++ hex_component/printHexBase.c
@@ -6,55 +6,58 @@
 /*   By: iyoshiha <iyoshiha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 04:03:35 by iyoshiha          #+#    #+#             */
-/*   Updated: 2021/11/24 01:50:00 by iyoshiha         ###   ########.fr       */
+/*   Updated: 2021/11/25 04:14:16 by iyoshiha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
-# include "../includes/printHexBase.h"
+#include <libft.h>
+#include <printHexBase.h>
 
-int		printHexBase(long long unsigned int decimal, char flag)
+int	printHexBase(long long unsigned int	decimal, char	flag)
 {
 	long long unsigned int	next_digit;
-	int remainder;
-	int printed_num;
+	int						remainder;
+	int						printed_num;
 
 	printed_num = 0;
 	if (decimal == 0)
-		return(print0());
-	if ((next_digit = (decimal / 16)) > 0)
+		return (print0());
+	next_digit = decimal / 16;
+	if (next_digit > 0)
 		printed_num += printHexBase(next_digit, flag);
 	remainder = decimal % 16;
 	if (remainder >= 10)
 		if (flag == 1)
 			remainder = replace_ten2a_capital(remainder);
-		else
-			remainder = replace_ten2a(remainder);
+	else
+		remainder = replace_ten2a(remainder);
 	else
 		remainder += '0';
 	ft_putchar_fd (remainder, STDOUT_FILENO);
 	printed_num++;
-	return(printed_num);
+	return (printed_num);
 }
 
-int		replace_ten2a(int num)
+int	replace_ten2a(int	num)
 {
-	int base;
+	int	base;
 
 	base = 'a' - 10;
 	return (base + num);
 }
 
-int		replace_ten2a_capital(int num)
+int	replace_ten2a_capital(int	num)
 {
-	int base;
+	int	base;
 
 	base = 'A' - 10;
 	return (base + num);
 }
-int		print0(void)
+
+int	print0(void)
 {
-	int		printed_num;
+	int	printed_num;
+
 	ft_putchar_fd('0', STDOUT_FILENO);
 	return (printed_num = 1);
 }
